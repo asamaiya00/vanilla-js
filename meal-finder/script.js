e540de203bb3277cfd2154974c641bc03da348cb
Animesh Samaiya <asamaiya00@gmail.com>
2021-09-25 15:56:04 +0530

    feat: single-meal-info

--- meal-finder/script.js
+++ meal-finder/script.js
@@ -40,5 +40,55 @@ function searchMeal(e) {
     alert("Please enter something.");
   }
 }
+function getSingleMeal(mealId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDOM(meal);
+    });
+}
+
+function addMealToDOM(meal) {
+  console.log(meal);
+  const ingredients = [];
+
+  for (i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]} `
+      );
+    } else {
+      break;
+    }
+  }
+  singleMealEl.innerHTML = `
+        <div class="single-meal">
+            <h2>${meal.strMeal}</h2>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+            <div class = "single-meal-info">
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+                ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+            </div>
+            <div class = "main">
+                <h2>Instructions</h2>
+                <p>${meal.strInstructions}</p>
+                <ul>
+                ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+                </ul>
+            </div>
+        </div>
+    `;
+}
 
 submit.addEventListener("submit", searchMeal);
+
+mealsEl.addEventListener("click", (e) => {
+  const mealInfo = e.path.find((meal) =>
+    meal.classList.contains("meal-info") ? meal : ""
+  );
+  const mealId = mealInfo ? mealInfo.getAttribute("data-mealid") : "";
+
+  getSingleMeal(mealId);
+});
