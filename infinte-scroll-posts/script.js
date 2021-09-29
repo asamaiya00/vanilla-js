bc1f4a4eca36ae6bc36046b080443cf208dad175
Animesh Samaiya <asamaiya00@gmail.com>
2021-09-29 17:51:50 +0530

    feat: fetch and show posts

---
+++ infinte-scroll-posts/script.js
@@ -0,0 +1,37 @@
+const postsContainer = document.getElementById("posts-container");
+const loader = document.querySelector(".loader");
+const filter = document.getElementById("filter");
+
+let limit = 5;
+let page = 1;
+
+const getPosts = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+  const data = await res.json();
+
+  return data;
+};
+
+async function showPosts() {
+  const posts = await getPosts();
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    console.log(posts);
+    postEl.classList.add("post");
+    postEl.innerHTML = `
+    <div class="number">${post.id}</div>
+    <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">
+        ${post.body}
+        </p>
+    </div>
+    `;
+
+    postsContainer.appendChild(postEl);
+  });
+}
+
+showPosts();
