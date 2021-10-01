e8b1a613c034bdbe8f5a13fd509f0b1e0d4e4c2e
Animesh Samaiya <asamaiya00@gmail.com>
2021-10-01 11:56:48 +0530

    feat: show text-box and fetch voices

--- text-speech-reader/script.js
+++ text-speech-reader/script.js
@@ -69,3 +69,29 @@ function createBox({ image, text }) {
 }
 
 data.forEach(createBox);
+
+let voices = [];
+
+function getVoices() {
+  voices = window.speechSynthesis.getVoices();
+
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+    voicesSelect.appendChild(option);
+  });
+}
+
+toggleBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.toggle("show")
+);
+
+closeBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.remove("show")
+);
+
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+
+// getVoices();
