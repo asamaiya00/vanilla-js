4b00c2a66a597717c57184677bd9cab926c3e440
Animesh Samaiya <asamaiya00@gmail.com>
2021-10-04 21:51:38 +0530

    feat: list songs and pagination

--- lyrics-search/script.js
+++ lyrics-search/script.js
@@ -2,12 +2,51 @@ const API_URL = "https://api.lyrics.ovh";
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const result = document.getElementById("result");
+const more = document.getElementById("more");
 
 async function fetchSongs(term) {
   const res = await fetch(`${API_URL}/suggest/${term}`);
-  const data = await res.json()
+  const data = await res.json();
 
-  console.log(data)
+  showDataInDOM(data);
+}
+async function fetchMoreSongs(url) {
+  console.log(url);
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showDataInDOM(data);
+}
+
+function showDataInDOM(data) {
+  result.innerHTML = `
+        <ul class="songs">
+        ${data.data
+          .map(
+            (song) =>
+              `<li>
+            <span><strong> ${song.title} </strong> - ${song.artist.name}</span>
+            <button class="btn" data-artist="${song.artist.name}" 
+            data-songtitle="${song.title}">Get lyrics</button>
+          </li>`
+          )
+          .join("")}
+        </ul>
+    `;
+  console.log(data.data[0]);
+  more.innerHTML = `
+    ${
+      data.prev
+        ? `<button onclick=fetchMoreSongs("${data.prev}") class='btn'>Prev</button>`
+        : ""
+    }
+    ${
+      data.next
+        ? `<button onclick=fetchMoreSongs("${data.next}") class='btn'>Next</button>`
+        : ""
+    }
+    `;
 }
 
 form.addEventListener("submit", (e) => {
