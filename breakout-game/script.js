cad3cc31d037d321b051234153c39ce31710a955
Animesh Samaiya <asamaiya00@gmail.com>
2021-10-06 20:59:40 +0530

    feat: draw bricks

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -6,6 +6,28 @@ const ctx = canvas.getContext("2d");
 
 let score = 0;
 
+const brickRows = 5;
+const brickColumns = 9;
+
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+const bricks = [];
+for (i = 0; i < brickColumns; i++) {
+  bricks[i] = [];
+  for (j = 0; j < brickRows; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
 rulesBtn.addEventListener("click", () => rules.classList.add("show"));
 closeBtn.addEventListener("click", () => rules.classList.remove("show"));
 
@@ -47,10 +69,24 @@ function drawScore() {
   ctx.font = "20px Arial";
   ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
 }
+
+function drawBricks() {
+  bricks.forEach((column) => {
+    column.forEach((block) => {
+      ctx.beginPath();
+      ctx.rect(block.x, block.y, block.w, block.h);
+      ctx.fillStyle = block.visible ? "lightseagreen" : "transparent";
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+}
+
 function draw() {
   drawBall();
   drawPaddle();
   drawScore();
+  drawBricks();
 }
 
-draw()
+draw();
